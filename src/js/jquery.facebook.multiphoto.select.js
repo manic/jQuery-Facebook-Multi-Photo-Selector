4a0f379729998d6da27d653f3f17f50e778749e0
Manic Chuang <manic.csie90@gmail.com>
2012-05-15 16:29:48 +0800

    chinese ver.

--- src/js/jquery.facebook.multiphoto.select.js
+++ src/js/jquery.facebook.multiphoto.select.js
@@ -1,7 +1,7 @@
 /**
  * jQuery Facebook Multi-Photo Selector (jfmps)
  * https://github.com/seanhellwig/jQuery-Facebook-Multi-Photo-Selector
- * Allows the selection of Facebook album photos 
+ * Allows the selection of Facebook album photos
  * @author Sean Hellwig
  * Inspired by: https://github.com/mbrevoort/jquery-facebook-multi-friend-selector - thanks for some css :)
  * @dependencies jQuery 1.4.4+ http://jquery.com/
@@ -17,18 +17,18 @@ window.log=function(){log.history=log.history||[];log.history.push(arguments);if
 		var container = $(element),
 			domStructure = container.append($('<div id="jfmps-inner-header">' +
 				'<div id="jfmps-breadcrumb">' +
-					'<span id="jfmps-default-crumb">Albums</span>' + 
-					'<span id="jfmps-crumb-separator">&raquo;</span>' + 
-					'<span id="jfmps-photos-list-crumb"></span>' + 
-				'</div>' + 
-				'<a class="jfmps-meta" id="jfmps-num-selected" href="javascript:void(0);">Selected (<span id="jfmps-selected-count">0</span>)</a>' + 
-				'<a class="jfmps-meta" id="jfmps-clear-button" href="javascript:void(0);">Clear Selected Images</a>' + 
-			'</div>' + 
-			'<div id="jfmps-album-covers"></div>' + 
-			'<div id="jfmps-album-photos"></div>' + 
-			'<div id="jfmps-selected-container">' + 
-				'<h3>Selected Photos</h3>' + 
-				'<div id="jfmps-selected-photos"></div>' + 
+					'<span id="jfmps-default-crumb">相簿</span>' +
+					'<span id="jfmps-crumb-separator">&raquo;</span>' +
+					'<span id="jfmps-photos-list-crumb"></span>' +
+				'</div>' +
+				'<a class="jfmps-meta" id="jfmps-num-selected" href="javascript:void(0);">已選擇(<span id="jfmps-selected-count">0</span>)</a>' +
+				'<a class="jfmps-meta" id="jfmps-clear-button" href="javascript:void(0);">清除已選相片</a>' +
+			'</div>' +
+			'<div id="jfmps-album-covers"></div>' +
+			'<div id="jfmps-album-photos"></div>' +
+			'<div id="jfmps-selected-container">' +
+				'<h3>已選相片</h3>' +
+				'<div id="jfmps-selected-photos"></div>' +
 			'</div>')),
 			albumContainer = $('#jfmps-album-covers', container),
 			albumPhotosContainer = $('#jfmps-album-photos', container),
@@ -53,7 +53,7 @@ window.log=function(){log.history=log.history||[];log.history.push(arguments);if
 			selectedPhotos,
 			selectedPhotosCount = 0;
 		settings = $.extend(true, settings, options || {});
-				
+
 		/**
 		 * Main entry point. Queries Facebook Graph API for a list of user albums, executes callback on completion.
 		*/
@@ -62,30 +62,30 @@ window.log=function(){log.history=log.history||[];log.history.push(arguments);if
 				if (settings.debug) {log('settings.numAlbumColumns & settings.numPhotosColumns must be greater than 0');}
 				return;
 			}
-			
+
 			_updateSelectedCountDisplay();
-			
+
 			FB.api('/me/albums', _showAlbumContent);
 		};
-		
-		/** 
+
+		/**
 		 * Parses raw Facebook Graph API album data, adds images to the DOM, sets up initial storage objects and event handlers.
 		 * @param response Facebook Response Object
 		*/
 		var _showAlbumContent = function(response){
 			if (settings.debug) {log('FB API Response /me/albums:', response);}
-			
+
 			if (response.data && response.data.length > 0) {
 				var albums = response.data,
 					i;
-				
+
 				albumImageCache = {};
 				selectedPhotos = {};
-				
+
 				breadcrumbEl.hide();
 				breadcrumbSeparator.hide();
 				albumPhotosContainer.hide();
-				
+
 				settings.imageSubmitButton.bind('click', _submitSelectedImages);
 				imageClearButton.bind('click', _clearSelectedImages);
 
@@ -101,7 +101,7 @@ window.log=function(){log.history=log.history||[];log.history.push(arguments);if
 		        container.append(content);
 			}
 		};
-		
+
 		/**
 		 * Helper method for _showAlbumContent. Creates the DOM structure for an Album image, adds click listener, and appends it to the container.
 		 * @param album Object
@@ -109,21 +109,21 @@ window.log=function(){log.history=log.history||[];log.history.push(arguments);if
 		var _appendAlbum = function(album){
 			var albumEl = $('<div id="fb-albumcover-' + album.id + '" class="jfmps-albumcover" />');
 			albumEl.data('album_id', album.id);
-			
+
 			var albumName = $('<div class="jfmps-albumname" />').text(album.name);
 			var albumCoverImage = $('<img width="50" src="https://graph.facebook.com/' + album.id  + '/picture?access_token=' + FB.getAuthResponse().accessToken + '&amp;type=thumbnail" />');
-			
+
 			albumEl.append(albumCoverImage);
-			
+
 			albumEl.append(albumName);
-			
+
 			albumEl.bind('click', function(e){
 				_handleAlbumClickListeners(e, albumEl, album.name);
 			});
-			
+
 			$('div.image-row', albumContainer).last().append(albumEl);
 		};
-		
+
 		/**
 		 * Click Event Handler for Albums. Queries Facebook Graph API for Album images, adds it to cache. Updates breadcrumb album name.
 		 * @param e Event Object.
@@ -133,11 +133,11 @@ window.log=function(){log.history=log.history||[];log.history.push(arguments);if
 		var _handleAlbumClickListeners = function(e, albumEl, albumName){
 			var albumId = albumEl.data('album_id');
 			breadcrumbEl.empty().html(breadCrumbTpl.replace('{album_name}', albumName));
-			
+
 			if (albumImageCache[albumId] === undefined) {
 				FB.api('/' + albumId + '/photos', function(response){
 					if (settings.debug) {log('FB API Response /' + albumId + '/photos:', response);}
-					
+
 					albumImageCache[albumId] = response.data;
 					_showAlbumImages(albumId);
 				});
@@ -145,7 +145,7 @@ window.log=function(){log.history=log.history||[];log.history.push(arguments);if
 				_showAlbumImages(albumId);
 			}
 		};
-		
+
 		/**
 		 * Helper method for _handleAlbumClickListeners.
 		 * Constructs DOM elements from cached Facebook Graph API Image Object. Adds Click Event Listener to breadcrumb bar.
@@ -162,18 +162,18 @@ window.log=function(){log.history=log.history||[];log.history.push(arguments);if
 					}
     				var imageContainer = $('<div id="fb-albumimage-' + albumImagesData[i].id + '" class="jfmps-albumimage" />');
     				imageContainer.data('image_id', albumImagesData[i].id);
-				
+
     				if (selectedPhotos[albumImagesData[i].id] !== undefined){
     					imageContainer.addClass('selected');
     				}
-				
+
     				var albumImage = $('<img width="75" src="https://graph.facebook.com/' + albumImagesData[i].id  + '/picture?access_token=' + FB.getAuthResponse().accessToken + '&amp;type=thumbnail" />');
-				
+
     				imageContainer.append(albumImage);
-				
+
     				$('div.image-row', albumPhotosContainer).last().append(imageContainer);
-				
-				
+
+
     				_makeImageSelectable(imageContainer, albumImagesData[i].images);
     			};
 		    }else{
@@ -185,7 +185,7 @@ window.log=function(){log.history=log.history||[];log.history.push(arguments);if
 			albumContainer.hide();
 			albumPhotosContainer.show();
 		};
-		
+
 		/**
 		 * Adds Click event listener to Album images.  Adds or removes an Album Image from the selectedItems list.
 		 * @param imageContainer jQuery Element - container the selected Image.
@@ -193,31 +193,31 @@ window.log=function(){log.history=log.history||[];log.history.push(arguments);if
 		**/
 		var _makeImageSelectable = function(imageContainer, imageData){
 			var imageId = imageContainer.data('image_id');
-			
+
 			imageContainer.bind('click', function(e){
 				if (selectedPhotos[imageId] === undefined) {
 					if (selectedPhotosCount < settings.maxPhotosSelected) {
 						selectedPhotos[imageId] = imageData;
 						imageContainer.addClass('selected');
-				
+
 						_addToSelectedList(imageContainer);
-				
+
 						selectedPhotosCount += 1;
 						_updateSelectedCountDisplay();
-						
+
 						settings.selectedImageCallback !== null ? settings.selectedImageCallback() : null;
 					}
 				}else{
 					imageContainer.removeClass('selected');
 					delete selectedPhotos[imageId];
-				
+
 					_removeFromSelectedList(imageContainer);
 				}
-			
+
 				_updateActionButtons();
 			});
 		};
-		
+
 		/**
 		 * Helper method for _makeImageSelectable. Adds Image to right column selected image list.
 		 * @param selectedImage jQuery Element - Image container.
@@ -227,46 +227,46 @@ window.log=function(){log.history=log.history||[];log.history.push(arguments);if
 				imageId = selectedImage.data('image_id'),
 				newImageItem = $('<li id="selected-' + imageId + '"/>'),
 				imageList;
-			
+
 			if ($('ul', selectedPhotosContainer).length <= 0){
 				imageList = $('<ul/>');
 				selectedPhotosContainer.append(imageList);
 			}else{
 				imageList = $('ul', selectedPhotosContainer);
 			}
-			
+
 			imageEl.clone().appendTo(newImageItem);
-			
+
 			imageList.append(newImageItem);
-			
+
 			_makeUnselectable(newImageItem);
 		};
-		
+
 		/**
 		 * Makes a selected image unselectable by addinga hover state and icon to unselect
 		 * @param listItem the <li> element that is to be made capable of unselecting
 		 **/
 		var _makeUnselectable = function(listItem){
 			var unselectEl = $('<span class="jfmps-selected-unselect"/>').html('[x] remove');
-			
+
 			unselectEl.hide();
-			
+
 			listItem.css('position', 'relative');
 			unselectEl.bind('click', function(e){
 				e.preventDefault();
 				e.stopPropagation();
-				
+
 				var imageId = listItem.attr('id').split('selected-')[1];
 				if (settings.debug) { log('deleted: ' + imageId); }
 				$('#fb-albumimage-' + imageId).removeClass('selected');
-				
+
 				delete selectedPhotos[imageId];
-				
+
 				listItem.remove();
-				
+
 				selectedPhotosCount -= 1;
 				_updateSelectedCountDisplay();
-			
+
 			});
 			listItem.bind('mouseenter', function(e){
 				unselectEl.show();
@@ -274,11 +274,11 @@ window.log=function(){log.history=log.history||[];log.history.push(arguments);if
 			listItem.bind('mouseleave', function(e){
 				unselectEl.hide();
 			});
-			
+
 			listItem.append(unselectEl);
-			
+
 		};
-		
+
 		/**
 		 * Helper method for  _makeImageSelectable. Removes Image from right column selected image list.
 		 * @param selectedImage jQuery Element - Image container.
@@ -286,14 +286,14 @@ window.log=function(){log.history=log.history||[];log.history.push(arguments);if
 		var _removeFromSelectedList = function(selectedImage){
 			var imageId = selectedImage.data('image_id'),
 				imageList = $('ul', selectedPhotosContainer).eq(0);
-				
+
 				$('li#selected-' + imageId, imageList).remove();
-				
+
 				selectedPhotosCount -= 1;
-				
+
 				_updateSelectedCountDisplay();
 		};
-		
+
 		/**
 		 * Empties Album Image container and hides it.  Hides breadcrumb bar. Shows Albums.
 		**/
@@ -303,7 +303,7 @@ window.log=function(){log.history=log.history||[];log.history.push(arguments);if
 			breadcrumbSeparator.hide();
 			albumContainer.show();
 		};
-		
+
 		/**
 		 * Creates JSON String of selectedPhotos Object
 		 * @return JSON String of keys - imageId, values 3 different size images.
@@ -315,7 +315,7 @@ window.log=function(){log.history=log.history||[];log.history.push(arguments);if
 				return false;
 			}
 		};
-		
+
 		/**
 		 * Send JSON String of selectedPhotos Object to callback function.
 		 * @param Event Object
@@ -323,38 +323,38 @@ window.log=function(){log.history=log.history||[];log.history.push(arguments);if
 		var _submitSelectedImages = function(e){
 			if (selectedPhotosCount > 0) {
 				var outputJson = _getSelectedImagesData();
-				
+
 				if (settings.debug) {log(outputJson);}
-				
+
 				if (settings.submitCallback) {
 					settings.submitCallback(outputJson);
 				};
 			};
 		};
-		
+
 		/**
 		 * Removed images from selectedPhotos Object and the selected images list. Toggles selected class on Image Container DOM elements.
 		**/
 		var _clearSelectedImages = function(){
 			for (var k in selectedPhotos){
 				if (selectedPhotos.hasOwnProperty(k)) {
-					
+
 					delete selectedPhotos[k];
-					
+
 					$('#fb-albumimage-' + k, container).removeClass('selected');
 					$('li#selected-' + k, container).remove();
-		
+
 				};
 			}
-			
+
 			selectedPhotosCount = 0;
-			
+
 			_updateSelectedCountDisplay();
-			
+
 			imageClearButton.hide();
 		};
-		
-		
+
+
 		/**
 		 * Toggles display state clear and submit button
 		**/
@@ -367,21 +367,21 @@ window.log=function(){log.history=log.history||[];log.history.push(arguments);if
 				imageClearButton.hide();
 			}
 		};
-		
+
 		/**
 		 * Updates the DOM element to show the current num of selected photos
 		**/
 		var _updateSelectedCountDisplay = function(){
 			countSelector.html(selectedPhotosCount + ' / ' + settings.maxPhotosSelected);
-			
+
 			_updateActionButtons();
 		};
-		
+
 		/**
 		 * Begin app
 		**/
 		_init();
-		
+
 		/**
 		 * Public API
 		*/
@@ -394,7 +394,7 @@ window.log=function(){log.history=log.history||[];log.history.push(arguments);if
 			}
 		};
 	};
-	
+
 	/**
 	 * jQuery plugin format
 	**/
@@ -402,15 +402,15 @@ window.log=function(){log.history=log.history||[];log.history.push(arguments);if
 		return this.each(function(){
 			var el = $(this),
 				jfmps;
-				
+
 			if (el.data('jfmps')) { return el.data('jfmps'); };
-			
+
 			jfmps = new JFMPS(this, options);
-			
+
 			el.data('jfmps', jfmps);
-			
+
 			return this;
 		});
 	};
-	
+
 })(jQuery);
